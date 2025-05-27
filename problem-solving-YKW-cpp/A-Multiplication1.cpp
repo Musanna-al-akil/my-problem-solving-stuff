a58c3d185f85bafda0a835e904fb5a7fb5bf1ab4
Musanna Al Akil <43068505+Musanna-al-akil@users.noreply.github.com>
2025-05-27 23:40:40 +0600

    solve 2 problem from atcoder

---
+++ problem-solving-YKW-cpp/A-Multiplication1.cpp
@@ -0,0 +1,15 @@
+// https://atcoder.jp/contests/abc169/tasks/abc169_a
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+	int a,b;
+
+	cin >> a >> b;
+
+	cout << a * b <<endl;
+
+	return 0;
+}
