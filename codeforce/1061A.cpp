2f1b5078d3d3dcbbbf22b32592343a844120c33b
Musanna Al Akil <43068505+Musanna-al-akil@users.noreply.github.com>
2025-11-03 23:23:01 +0600

    Add: new Codeforces solutions for problems 1061A and 1454A

---
+++ codeforce/1061A.cpp
@@ -0,0 +1,16 @@
+// https://codeforces.com/problemset/problem/1061/A
+
+// Author: Musanna Al Akil
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+   int n, s;
+   cin >> n>>s;
+
+
+   cout << (long long)ceil((double)s/n)<<endl;
+   
+   return 0;
+}
---
+++ codeforce/1454A.cpp
@@ -0,0 +1,26 @@
+// https://codeforces.com/problemset/problem/1454/A
+
+// Author: Musanna Al Akil
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base
+   int t;
+   cin >> t;
+
+   while(t--){
+   	int n;
+   	cin >> n;
+
+   	cout << n<<' ';
+
+   	for(int i =1;i<n;i++){
+   		cout << i << ' ';
+   	}
+   	cout << '\n';
+   }
+   
+   return 0;
+}
